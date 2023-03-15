077f93c2acd018895107b0ff9c67bc67010415a3
erasmus95 <36201011+erasmus95@users.noreply.github.com>
2023-03-15 00:31:28 -0400

    Update mill.py

--- code/mill.py
+++ code/mill.py
@@ -1,29 +1,36 @@
 import serial
 import time
 
-ser = serial.Serial(
-    port='/dev/ttyUSB0',
-    baudrate=115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-)
+# configure the serial port
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.port = '/dev/ttyUSB0'  # set your port name here
+ser.timeout = 1
+ser.open()
 
+# define some commands
+home = "G28\n"  # home all axes
+move_to = "G1 X{} Y{} Z{}\n"  # move to specified coordinates
 
-time.sleep(2)
-command = 'G0x-20 0\n'# G00X80Y20\n'
-#command = '(ctrl-x)\n'
-print(f'Sending: {command}')
-ser.write(command.replace(" ","").encode())
+# move to the starting position
+ser.write(home.encode())
+response = ser.readline().decode().strip()  # read the response
+print(response)
+time.sleep(5)  # wait for 5 seconds for the machine to home
 
-time.sleep(1)
-#grbl_out = ser.readline()
-#print(grbl_out)
-out=''
-while ser.inWaiting() > 0:
-    out = ser.readline()
-            
-    if out != '':
-        print(out.strip().decode())
+# move to the target position
+x, y, z = 40, 30, -20  # set target coordinates
+ser.write(move_to.format(x, y, z).encode())
+response = ser.readline().decode().strip()  # read the response
+print(response)
 
+# wait for the machine to finish moving
+time.sleep(10)  # wait for 10 seconds
+
+# stop the machine
+ser.write(b'\x18')  # send the keyboard interrupt signal to stop the machine
+response = ser.readline().decode().strip()  # read the response
+print(response)
+
+# close the serial port
 ser.close()
