98a0185f8a8d9dd748c71d2e943f4fd72a8f9354
erasmus95 <36201011+erasmus95@users.noreply.github.com>
2023-03-14 20:02:08 -0400

    clean up

--- code/testing.py
+++ code/testing.py
@@ -1,27 +1,2 @@
-import time,serial
-from serial import tools
+import NESP-Lib
 
-#Alphabetically lists the ports by name
-def comslist():
-    ports = []
-    for i in serial.tools.list_ports.comports():
-        try:
-            ser = serial.Serial(i.name)
-            ser.close()
-        except serial.SerialException as e:
-            print(e)
-        else:
-            ports.append(i.name)
-    ports.sort()
-    return ports
-
-#Finds the desired port using the name eg COM1
-def selectcom(port):
-    try :
-        ser = serial.Serial(port)
-    except serial.SerialException as e:
-        print(e)
-    else:
-        return ser
-
-comslist()
