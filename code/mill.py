af0302928ecab3e697c197f96e3937b2dbf1850f
erasmus95 <36201011+erasmus95@users.noreply.github.com>
2023-03-15 00:33:37 -0400

    Update mill.py
    Adding functions

--- code/mill.py
+++ code/mill.py
@@ -10,25 +10,33 @@ ser.open()
 
 # define some commands
 home = "G28\n"  # home all axes
-move_to = "G1 X{} Y{} Z{}\n"  # move to specified coordinates
+move = "G1 X{} Y{} Z{}\n"  # move to specified coordinates
+
+# define reusable functions
+def send_command(command):
+    ser.write(command.encode())
+    response = ser.readline().decode().strip()
+    return response
+
+def move_to_position(x, y, z):
+    command = move.format(x, y, z)
+    response = send_command(command)
+    return response
 
 # move to the starting position
-ser.write(home.encode())
-response = ser.readline().decode().strip()  # read the response
-print(response)
+send_command(home)
 time.sleep(5)  # wait for 5 seconds for the machine to home
 
 # move to the target position
 x, y, z = 40, 30, -20  # set target coordinates
-ser.write(move_to.format(x, y, z).encode())
-response = ser.readline().decode().strip()  # read the response
+response = move_to_position(x, y, z)
 print(response)
 
 # wait for the machine to finish moving
 time.sleep(10)  # wait for 10 seconds
 
 # stop the machine
-ser.write(b'\x18')  # send the keyboard interrupt signal to stop the machine
+send_command(b'\x18')
 response = ser.readline().decode().strip()  # read the response
 print(response)
 
