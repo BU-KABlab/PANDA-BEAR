f4698a04d8fd26dc9fbcaa705e492a824b503cb7
erasmus95 <36201011+erasmus95@users.noreply.github.com>
2023-06-02 10:53:44 -0400

    Update scratch.py

--- code/demo/scratch.py
+++ code/demo/scratch.py
@@ -1,4 +1,5 @@
-from classes import Vial, Wells
+from classes import Vial, Wells, MillControl
+import serial
 
 v1 = Vial(-10, -10, -10, -30, "water", 100)
 v2 = Vial(-10, -20, -10, -30, 'bleach',150)
@@ -13,3 +14,17 @@ wells.visualize_well_coordinates()
 #mill_move = "G0 X{} Y{} Z{}"  # move to specified coordinates
 #command = mill_move.format(v1.position['x'],v1.position['y'],v1.position['z'])
 #print(command)
+def SET_UP_MILL():
+    ser_mill = serial.Serial(
+        port= 'COM4',
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+        )
+    return ser_mill
+serial_mill = SET_UP_MILL()
+mill = MillControl(serial_mill)
+mill.__enter__
+mill.home
