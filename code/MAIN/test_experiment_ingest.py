2902b062ce7dceb2c6b0a2414632545941c9b740
erasmus95 <36201011+erasmus95@users.noreply.github.com>
2023-08-04 16:37:23 -0400

    Individual experiments
    Now experiments are read in individually and save as separate files with the date and the well they are assigned to. These are looped through, calling run_experiments.py for each one. As the experiments run, the well_status.json and the experiment.json files are updated.
    
    When reading in experiments, the desired well is checked against well_status.json and if that well is not available the next available well is assigned to the experiment. If there are no available wells the instructions are not moved to archive and a print out tells the user that not all experiments have been assigned.
    
    Additionally changes were made to add functions to allow all of this as well as folders for organization

--- code/MAIN/test_experiment_ingest.py
+++ code/MAIN/test_experiment_ingest.py
@@ -1,4 +1,4 @@
-import json, pathlib, datetime
+import json, pathlib, datetime, os
 
 def check_well_status(well: str):
     """
@@ -42,7 +42,7 @@ def change_well_status(well: str, status: str):
         for wells in data['Wells']:
             if wells['Target_Well'] == well:
                 wells['status'] = status
-                wells['status_date'] = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')
+                wells['status_date'] = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
                 break
     with open(file_to_open, "w") as file:
         json.dump(data, file, indent=4)
@@ -53,12 +53,17 @@ def read_new_experiments(filename: str):
     :param filename: The name of the JSON file to read.
     :return: The data from the JSON file as a dictionary.
     """
+    experiments_read = 0
+    complete = True
     cwd = pathlib.Path(__file__).parents[0]
-    file_path = cwd / "inbox"
+    file_path = cwd / "experiments_inbox"
     file_to_open = file_path / filename
     with open(file_to_open, "r", encoding="ascii") as file:
         data = json.load(file)
         for experiment in data['Experiments']:
+            existing_status = experiment['status']
+            if existing_status != "new":
+                continue
             # Get the target well and create a filename
             desired_well = experiment['Target_Well']
 
@@ -68,41 +73,130 @@ def read_new_experiments(filename: str):
                 target_well = choose_alternative_well(desired_well)
                 if target_well == "none":
                     print(f"No wells available for experiment originally for well {desired_well}.")
+                    complete = False
                     continue
-                print(f"Experiment originally for well {desired_well} is now for well {target_well}.")                
+                else:
+                    print(f"Experiment originally for well {desired_well} is now for well {target_well}.")
+                    experiment['Target_Well'] = target_well                
             else:
                 target_well = desired_well
 
-            filename = f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}_{target_well}.json"
+            filename = f"{datetime.datetime.now().strftime('%Y-%m-%d_%H')}_{target_well}.json"
+
+            # add additional information to the experiment
+            experiment['status_date'] = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
+            experiment["time_stamps"] = []
+            experiment["OCP_file"] = ""
+            experiment["OCP_pass"]= None
+            experiment["deposition_data_file"] = ""
+            experiment["deposition_plot_file"] = ""
+            experiment["characterization_data_file"] = ""
+            experiment["characterization_plot_file"] = ""
+
 
             # Save the experiment as a separate file in the experiment_que subfolder
-            subfolder_path = cwd / "experiment_que"
+            subfolder_path = cwd / "experiment_queue"
             subfolder_path.mkdir(parents=True, exist_ok=True)
             file_to_save = subfolder_path / filename
             with open(file_to_save, "w") as outfile:
                 json.dump(experiment, outfile, indent=4)
 
             # Change the status of the well
-            change_well_status(target_well, "qued")
+            change_well_status(target_well, "queued")
+            experiment['status'] = "queued"
 
-            # Delete the experiment from the inbox
-            file_to_open.unlink()
+            # Add the experiment to the list of experiments read
+            experiments_read += 1
+        
+    # Save the updated file
+    with open(file_to_open, "w") as file:
+        json.dump(data, file, indent=4)
 
-    return [experiment['Target_Well'] for experiment in data['Experiments']]
+
+
+    return experiments_read, complete
 
 def check_inbox():
     """
-    Checks the inbox for new experiments.
+    Checks the experiments inbox folder for new experiments.
     :return: the filename(s) of new experiments.
     """
     cwd = pathlib.Path(__file__).parents[0]
-    file_path = cwd / "inbox"
+    file_path = cwd / "experiments_inbox"
+    count = 0
     for file in file_path.iterdir():
         if file.is_file():
-            return read_new_experiments(file.name)
+            [count,complete] = read_new_experiments(file.name)
+
+            # Move to archive the file if it has been read
+            if complete:
+                archive_path = file_path / "archive"
+                archive_path.mkdir(parents=True, exist_ok=True)
+                file.replace(archive_path / file.name)
+                print(f"File {file.name} moved to archive.")
+            else:
+                print(f"File {file.name} not moved to archive. Not all experiments read.")
+
+    return count
         
+def read_next_experiment_from_queue():
+    """
+    Reads the next experiment from the queue.
+    :return: The next experiment.
+    """
+    cwd = pathlib.Path(__file__).parents[0]
+    file_path = cwd / "experiment_queue"
+    
+    ## if folder is not empty pick the first file in the queue
+    if os.listdir(file_path):
+        file_to_open = file_path / os.listdir(file_path)[0]
+        with open(file_to_open, "r", encoding="ascii") as file:
+            data = json.load(file)
+        return data, file_to_open
+    else:
+        return None, None
+
+def save_completed_instructions(instructions: list, filename: str):
+    '''Save the experiment instructions to either the completed or failed instructions folder.
+        Delete the experiment from the instructions queue'''
+    filename = filename.name
+    cwd = pathlib.Path(__file__).parents[0]
+    queue_file_path = cwd / "experiment_queue"
+    completed_file_path = cwd / "experiments_completed"
+    failed_file_path = cwd / "experiments_error"
+
+    if instructions['status'] == "completed":
+        file_to_save = completed_file_path / (filename)
+    elif instructions['status'] == "error":
+        file_to_save = failed_file_path / (filename)
+    else:
+        return False
+        
+    with open(file_to_save, "w") as f:
+        json.dump(instructions, f, indent=4)
+        print(f"Experiment {filename} saved to {file_to_save}.")
+
+    os.remove(queue_file_path / (filename))
+    print(f"Experiment {filename} removed from queue.")
 
 if __name__ == "__main__":
-    check_inbox()
+    print(f"{check_inbox()} new experiments read.")
 
-    
+    exp, filename = read_next_experiment_from_queue()
+    runtimes = {
+        "Start Time": 1690497089,
+        "Solutions Time": 1690497656,
+        "Deposition Time": 1690497996,
+        "Rinse Time": 1690499112,
+        "Characterization Time": 1690499549,
+        "Clear Well Time": 1690499769,
+        "Flush Time": 1690499875,
+        "End Time": 1690499875,
+        "Well Time": 167104.380354881
+    }
+    exp["time_stamps"] = runtimes
+    exp['status'] = "error"
+    exp['status_date'] = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
+    print(json.dumps(exp, indent=4))
+
+    save_completed_instructions(exp, filename)
