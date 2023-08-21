bc2c3304b1e36be44391fdb8739984bf391b2d98
erasmus95 <36201011+erasmus95@users.noreply.github.com>
2023-08-21 16:01:43 -0400

    add slack file function

--- code/MAIN/slack_functions.py
+++ code/MAIN/slack_functions.py
@@ -37,6 +37,40 @@ def send_slack_message(channel: str, message):
     except SlackApiError as error:
         logging.error("Error posting message: %s", error)
 
+
+def send_slack_file(channel: str, file, message = None):
+    '''Send a file to a Slack channel using the Slack API.'''
+    # Get image file name
+    filename_to_post = file
+
+    # WebClient insantiates a client that can call API methods
+    # When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+    client = WebClient(slack_cred.token)
+    # The name of the file you're going to upload
+    file_name = "./TestImage.gif"
+    # ID of channel that you want to upload file to
+    if channel == 'conversation':
+        channel_id = slack_cred.conversation_channel_id
+    elif channel == 'alert':
+        channel_id = slack_cred.alert_channel_id
+    else:
+        return 'No applicable channel'
+
+    try:
+        # Call the files.upload method using the WebClient
+        # Uploading files requires the `files:write` scope
+        result = client.files_upload(
+            channels=channel_id,
+            file=filename_to_post,
+            filename=file_name,
+            filetype="gif",
+            initial_comment=message
+        )
+        # Log the result
+        print(result)
+
+    except SlackApiError as e:
+        print("Error uploading file: {}".format(e))
 if __name__ == "__main__":
     MESSAGE = "This is a test message."
     send_slack_message('alert', MESSAGE)
