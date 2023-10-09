1c8bc18807d8dec797e802a93b7ec22641888897
Panda-Station <36201011+erasmus95@users.noreply.github.com>
2023-10-08 22:56:34 -0400

    move reset well status to scheduler

--- code/scheduler.py
+++ code/scheduler.py
@@ -356,6 +356,30 @@ class Scheduler:
         with open(pathlib.Path.cwd() / "data" / f"{experiment.id}.json", "w", encoding= 'UTF-8') as results_file:
             results_file.write(results_json)
 
+
+    def reset_well_statuses(self):
+        """Loop through the well statuses and set them all to new"""
+        well_status_file = pathlib.Path(__file__).parents[0] / "well_status copy.json"
+        # input("This will reset all well statuses to new. Press enter to continue.")
+
+        # Confirm that the user wants this
+        choice = input(
+        "This will reset all well statuses to new. Press enter to continue. Or enter 'n' to cancel: "
+        )
+        if choice == "n":
+            print("Exiting program.")
+            return 0
+        with open(well_status_file, "r", encoding="UTF-8") as file:
+            well_status = json.load(file)
+        for catergory in well_status:
+            for well in well_status[catergory]:
+                well["status"] = "new"
+        with open(well_status_file, "w", encoding="UTF-8") as file:
+            json.dump(well_status, file, indent=4)
+
+        print("Well statuses reset to new.")
+        return 0
+
 ####################################################################################################
 def test_well_status_update():
     """
