e69e1c949471ff4672434ed451cacf762e7d8853
Panda-Station <36201011+erasmus95@users.noreply.github.com>
2023-10-08 22:56:47 -0400

    moving unused code

--- code/scratch.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-import typing
-from pydantic import ConfigDict, FilePath
-print()
-print()
-## get the current directory
-desired = pathlib.Path.cwd() / 'code/config' / 'mill_config.json'
-isinstance(desired, FilePath)
