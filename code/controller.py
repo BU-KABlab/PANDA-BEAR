81729d14aef242f87402822759d1903ac3bb3bd8
Panda-Station <36201011+erasmus95@users.noreply.github.com>
2023-10-29 13:40:43 -0400

    increase the wait time while idle to 5 min

--- code/controller.py
+++ code/controller.py
@@ -79,9 +79,9 @@ def main():
                 slack.send_slack_message("alert", "No new experiments to run...monitoring inbox for new experiments")
             while new_experiment is None:
                 logger.info(
-                    "No new experiments to run...waiting 1 minute for new experiments"
+                    "No new experiments to run...waiting 5 minutes for new experiments"
                 )
-                time.sleep(60)
+                time.sleep(600)
                 # Replace with slack alert and wait for response from user
                 scheduler.check_inbox()
                 new_experiment, _ = scheduler.read_next_experiment_from_queue()
