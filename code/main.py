ddf59d7939fbeda452cfcba7cdf3b12d7c38d905
Panda-Station <36201011+erasmus95@users.noreply.github.com>
2023-11-10 11:26:39 -0500

    better menu

--- code/main.py
+++ code/main.py
@@ -1,55 +1,67 @@
-"""Starts ePANDA."""
-
 import controller
+import sys
+
+def run_epanda():
+    """Runs ePANDA."""
+    controller.main()
+
+def run_epanda_mock():
+    """Runs ePANDA with mock instruments."""
+    controller.main(use_mock_instruments=True)
+
+def change_wellplate():
+    """Changes the current wellplate."""
+    new_wellplate_id = input("Enter the new wellplate ID: ")
+    new_wellplate_type = input("Enter the new wellplate type #: ")
+    controller.load_new_wellplate(new_wellplate_id, new_wellplate_type)
+
+def reset_vials_stock():
+    """Resets the stock vials."""
+    controller.reset_vials('stock')
+
+def reset_vials_waste():
+    """Resets the waste vials."""
+    controller.reset_vials('waste')
+
+def input_new_vial_values_stock():
+    """Inputs new values for the stock vials."""
+    controller.input_new_vial_values('stock')
+
+def input_new_vial_values_waste():
+    """Inputs new values for the waste vials."""
+    controller.input_new_vial_values('waste')
+
+def change_wellplate_location():
+    """Changes the location of the current wellplate."""
+    controller.change_wellplate_location()
+
+def exit_program():
+    """Exits the program."""
+    print("Exiting ePANDA. Goodbye!")
+    sys.exit()
+
+options = {
+    '0': run_epanda,
+    '1': run_epanda_mock,
+    '2': change_wellplate,
+    '3': reset_vials_stock,
+    '4': reset_vials_waste,
+    '5': input_new_vial_values_stock,
+    '6': input_new_vial_values_waste,
+    '7': change_wellplate_location,
+    '12': exit_program  # changed from 'exit' to 'exit_program'
+}
 
 if __name__ == "__main__":
     print("Welcome to ePANDA!")
-    response = input("Do you want to run the main ePANDA program? (y/n) ")
-    if response.lower() == 'y':
-        controller.main()
-    else:
-        while True:
-            print("What would you like to do?")
-            print("0. Run the main ePANDA program")
-            print("1. Change the wellplate")
-            print("2. Input new stock vials")
-            print("3. Reset the waste vials")
-            print("4. Input new stock vials")
-            print("5. Input new waste vials")
-            print("06. Change the position of the wellplate")
-            # print("07. Change the position of the pipette")
-            # print("08. Change the position of the electrode")
-            # print("09. Change the position of the camera")
-            # print("10. Change the position of solvent bath")
-            print("11. Exit")
+    while True:
+        print("What would you like to do?")
+        for key, value in options.items():
+            print(f"{key}. {value.__name__.replace('_', ' ').title()}")
 
-            user_choice = input("Enter the number of your choice: ")
-            if user_choice == '0':
-                controller.main()
-            elif user_choice == '1':
-                new_wellplate_id = input("Enter the new wellplate ID: ")
-                new_wellplate_type = input("Enter the new wellplate type #: ")
-                controller.load_new_wellplate(new_wellplate_id, new_wellplate_type)
-            elif user_choice == '2':
-                controller.reset_vials('stock')
-            elif user_choice == '3':
-                controller.reset_vials('waste')
-            elif user_choice == '4':
-                controller.input_new_vial_values('stock')
-            elif user_choice == '5':
-                controller.input_new_vial_values('waste')
-            elif user_choice == '6':
-                controller.change_wellplate_location()
-            # elif user_choice == '7':
-            #     controller.change_pipette()
-            # elif user_choice == '8':
-            #     controller.change_electrode()
-            # elif user_choice == '9':
-            #     controller.change_camera()
-            # elif user_choice == '10':
-            #     controller.change_solvent_bath()
-            elif user_choice == '11':
-                break
-            else:
-                print("Invalid choice. Please try again.")
-                continue
+        user_choice = input("Enter the number of your choice: ")
+        if user_choice in options:
+            options[user_choice]()
+        else:
+            print("Invalid choice. Please try again.")
+            continue
