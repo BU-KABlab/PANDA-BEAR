a8e1711e133a2f3e970c723e1101532a19e64307
erasmus95 <36201011+erasmus95@users.noreply.github.com>
2023-11-20 23:12:22 -0500

    cleaning up

--- code/water test experiments.py
+++ code/water test experiments.py
@@ -3,7 +3,6 @@ Using a 96 well plate, generate experiments to test the water
 Each column is a replicate and every row is a different volume
 
 """
-
 import experiment_class
 #import numpy as np
 #import matplotlib.pyplot as plt
@@ -12,27 +11,34 @@ from scheduler import Scheduler
 import controller
 import pandas as pd
 from pathlib import Path
+
+# Shelve current wellplate (unless new) and load new wellplate and reset vials
 controller.load_new_wellplate()
 controller.reset_vials("stock")
 controller.reset_vials("waste")
+
+# Load well history to get last experiment id
 well_hx = pd.read_csv(Path.cwd() / 'data' / 'well_history.csv', skipinitialspace=True)
 well_hx = well_hx.dropna(subset=['experiment id'])
 well_hx = well_hx.drop_duplicates(subset=['experiment id'])
 well_hx = well_hx[well_hx['experiment id'] != 'None']
 well_hx['experiment id'] = well_hx['experiment id'].astype(int)
 last_experiment_id = well_hx['experiment id'].max()
+experiment_id = int(last_experiment_id+1)
+
+# Create experiments
 COLUMNS = 'ABCDEFGH'
 ROWS = 12
-experiment_id = int(last_experiment_id+1)
 PROJECT_ID = 6
 EXPERIMENT_NAME = 'Water test'
+PROJECT_CAMPAIGN_ID = 1
 experiments = []
-# Create a new experiment
+# Create 96 new experiemnts for water
 for column in COLUMNS:
-#for column in 'A':
+#ex: for column in 'A':
     volume = 130
     for row in range(1,ROWS+1):
-    #for row in range(1,13):
+    #ex: for row in range(1,13):
         experiments.append(experiment_class.ExperimentBase(
                 id=experiment_id,
                 experiment_name= EXPERIMENT_NAME,
@@ -40,7 +46,7 @@ for column in COLUMNS:
                 target_well= column + str(row),
                 pin = CURRENT_PIN,
                 project_id=PROJECT_ID,
-                project_campaign_id=6,
+                project_campaign_id=PROJECT_CAMPAIGN_ID,
                 solutions={'water': volume},
                 status='new',
                 filename='water_test_' + str(experiment_id),
@@ -55,23 +61,3 @@ if result == 'success':
     controller.main(use_mock_instruments=True)
 else:
     print('Error: ', result)
-
-# Create a dataframe to store the results
-#results = pd.DataFrame(columns=['experiment_id', 'volume', 'mass'])
-
-# # Run the experiments
-# for experiment in experiments:
-#     #experiment.run()
-#     results = results.append({'experiment_id': experiment.id,
-#                               'volume': experiment.volume,
-#                               'mass': experiment.mass},
-#                              ignore_index=True)
-
-# # Plot the results
-# plt.figure()
-# plt.plot(results['volume'], results['mass'], 'o')
-# plt.xlabel('Volume (mL)')
-# plt.ylabel('Mass (g)')
-# plt.title('Mass of water in 96 well plate')
-# plt.show()
-# plt.savefig('water_test.png')
