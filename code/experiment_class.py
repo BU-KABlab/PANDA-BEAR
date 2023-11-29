1ce1fd0b7b9a3b3b0b13d6fcab6dcc8616825610
erasmus95 <36201011+erasmus95@users.noreply.github.com>
2023-11-29 10:31:30 -0500

    add tests for different experiment types

--- code/experiment_class.py
+++ code/experiment_class.py
@@ -398,10 +398,48 @@ def test_parse():
     print(f"Parsed--->{parsed_value}")
     assert value == parsed_value
 
-def test_serialize():
+def test_serialize_experiment():
     '''Test that the class can be serialized to json'''
     value = make_test_value()
     sample_json = RootModel[Experiment](value).model_dump_json(indent=4)
+    print(sample_json)
+    with open('temp_test_file.json', 'w', encoding='UTF-8') as f:
+        f.write(sample_json)
+
+def test_serialize_experimentbase():
+    '''Test that the class can be serialized to json'''
+    value = make_test_base_value()
+    sample_json = RootModel[ExperimentBase](value).model_dump_json(indent=4)
+    print(sample_json)
+    with open('temp_test_file.json', 'w', encoding='UTF-8') as f:
+        f.write(sample_json)
+
+def test_serialize_layered_experiment():
+    '''Test that the class can be serialized to json'''
+    value = make_test_layered_value()
+    sample_json = RootModel[LayeredExperiments](value).model_dump_json(indent=4)
+    print(sample_json)
+    with open('temp_test_file.json', 'w', encoding='UTF-8') as f:
+        f.write(sample_json)
+
+def test_serialize_results():
+    '''Test that the class can be serialized to json'''
+    value = ExperimentResult(
+        id=0,
+        well_id="D5",
+        ocp_dep_file="ocp_dep_file",
+        ocp_dep_pass=True,
+        ocp_char_file="ocp_char_file",
+        ocp_char_pass=True,
+        deposition_data_file="deposition_data_file",
+        deposition_plot_file="deposition_plot_file",
+        deposition_max_value=0.0,
+        depsotion_min_value=0.0,
+        characterization_data_file="characterization_data_file",
+        characterization_plot_file="characterization_plot_file",
+        characterization_max_value=0.0,
+        characterization_min_value=0.0)
+    sample_json = RootModel[ExperimentResult](value).model_dump_json(indent=4)
     print(sample_json)
     with open('temp_test_file.json', 'w', encoding='UTF-8') as f:
         f.write(sample_json)
@@ -412,6 +450,6 @@ def test_schema():
     print(json.dumps(TypeAdapter(Experiment).json_schema(), indent=4))
 
 if __name__ == "__main__":
-    test_serialize()
+    test_serialize_experimentbase()
     test_parse()
     test_schema()
