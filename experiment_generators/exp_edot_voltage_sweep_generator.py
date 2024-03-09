0dac031f910da165f3a1bad547668b8b3a220e5f
Greg-Desktop <36201011+erasmus95@users.noreply.github.com>
2024-03-09 08:37:01 -0500

    Refactor experiment generator function, adding main function to be a the common reference point for other scripts

--- experiment_generators/exp_edot_voltage_sweep_generator.py
+++ experiment_generators/exp_edot_voltage_sweep_generator.py
@@ -10,44 +10,51 @@ from epanda_lib.scheduler import Scheduler, determine_next_experiment_id
 
 TEST: bool = TESTING
 
-# Create experiments
-PROJECT_ID: int = 16
-EXPERIMENT_NAME = "edot potential screening"
-CAMPAIGN_ID: int = 2
-PUMPING_RATE: float = 0.3
-REPITITIONS: int = 3
 
-
-def main(starting_well: str = "F2"):
+def main(
+    project_id: int = None,
+    experiment_name: str = None,
+    campaign_id: int = None,
+    pumping_rate: float = None,
+    repetitions: int = None,
+    starting_well: str = "F2",
+):
     """
     Generate the experiments for the edot voltage sweep
     """
     print("TEST MODE: ", TEST)
     input("Press enter to continue")
+    project_id: int = 16
+    experiment_name = "edot potential screening"
+    campaign_id: int = 2
+    pumping_rate: float = 0.3
+    repetitions: int = 1
+    well_number: int = int(starting_well[1:])
+    well_letter: str = starting_well[0]
+
     ca_step_1_voltages = [0.8, 1.0, 1.2, 1.4, 1.6]
     wellplate.load_new_wellplate(False, 107, 4)
     experiment_id = determine_next_experiment_id()
     experiments: list[experiment_class.EchemExperimentBase] = []
-    well_number: int = int(starting_well[1:])
-    well_letter: str = starting_well[0]
 
-    for _ in range(REPITITIONS):
+    for _ in range(repetitions):
 
         for dep_v in ca_step_1_voltages:
             experiments.append(
                 experiment_class.EchemExperimentBase(
                     id=experiment_id,
+                    protocol_id=10,
                     well_id=str(well_letter) + str(well_number),
-                    experiment_name=EXPERIMENT_NAME + " " + "deposition",
+                    experiment_name=experiment_name + " " + "deposition",
                     priority=1,
                     pin=CURRENT_PIN,
-                    project_id=PROJECT_ID,
-                    project_campaign_id=CAMPAIGN_ID,
+                    project_id=project_id,
+                    project_campaign_id=campaign_id,
                     solutions={"edot": 120, "liclo4": 0, "rinse": 120},
                     solutions_corrected={"edot": 0, "liclo4": 0, "rinse": 0},
-                    pumping_rate=PUMPING_RATE,
+                    pumping_rate=pumping_rate,
                     status=experiment_class.ExperimentStatus.NEW,
-                    filename=EXPERIMENT_NAME + " " + str(experiment_id),
+                    filename=experiment_name + " " + str(experiment_id),
                     override_well_selection=0,  # 0 to use new wells only, 1 to reuse a well
                     process_type=1,
                     # Echem specific
@@ -70,17 +77,18 @@ def main(starting_well: str = "F2"):
             experiments.append(
                 experiment_class.EchemExperimentBase(
                     id=experiment_id,
+                    protocol_id=10,
                     well_id=str(well_letter) + str(well_number),
-                    experiment_name=EXPERIMENT_NAME + " " + "bleaching",
+                    experiment_name=experiment_name + " " + "bleaching",
                     priority=1,
                     pin=CURRENT_PIN,
-                    project_id=PROJECT_ID,
-                    project_campaign_id=CAMPAIGN_ID,
+                    project_id=project_id,
+                    project_campaign_id=campaign_id,
                     solutions={"edot": 0, "liclo4": 120, "rinse": 0},
                     solutions_corrected={"edot": 0, "liclo4": 0, "rinse": 0},
-                    pumping_rate=PUMPING_RATE,
+                    pumping_rate=pumping_rate,
                     status=experiment_class.ExperimentStatus.NEW,
-                    filename=EXPERIMENT_NAME + " " + str(experiment_id),
+                    filename=experiment_name + " " + str(experiment_id),
                     override_well_selection=1,
                     process_type=2,
                     # Echem specific
@@ -103,17 +111,18 @@ def main(starting_well: str = "F2"):
             experiments.append(
                 experiment_class.EchemExperimentBase(
                     id=experiment_id,
+                    protocol_id=10,
                     well_id=str(well_letter) + str(well_number),
-                    experiment_name=EXPERIMENT_NAME + " " + "coloring",
+                    experiment_name=experiment_name + " " + "coloring",
                     priority=1,
                     pin=CURRENT_PIN,
-                    project_id=PROJECT_ID,
-                    project_campaign_id=CAMPAIGN_ID,
+                    project_id=project_id,
+                    project_campaign_id=campaign_id,
                     solutions={"edot": 0, "liclo4": 120, "rinse": 120},
                     solutions_corrected={"edot": 0, "liclo4": 0, "rinse": 0},
-                    pumping_rate=PUMPING_RATE,
+                    pumping_rate=pumping_rate,
                     status=experiment_class.ExperimentStatus.NEW,
-                    filename=EXPERIMENT_NAME + " " + str(experiment_id),
+                    filename=experiment_name + " " + str(experiment_id),
                     override_well_selection=1,
                     process_type=3,
                     # Echem specific
@@ -140,7 +149,7 @@ def main(starting_well: str = "F2"):
         print(f"Experiment id: {experiment.id}")
         print(f"Well id: {experiment.well_id}")
         print(f"Solutions: {json.dumps(experiment.solutions)}")
-        print(f"Pumping rate: {PUMPING_RATE}")
+        print(f"Pumping rate: {pumping_rate}")
         print(
             f"Project campaign id: {experiment.project_id}.{experiment.project_campaign_id}\n"
         )
