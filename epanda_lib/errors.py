fd8e7c66237cc5966ff5d38c22bbdbb726f04833
Greg-Desktop <36201011+erasmus95@users.noreply.github.com>
2024-05-16 20:58:14 -0400

    consolidate more errors into errors.py

--- epanda_lib/errors.py
+++ epanda_lib/errors.py
@@ -86,3 +86,31 @@ class CVFailure(Exception):
         self.well_id = well_id
         self.message = f"CV failed for experiment {experiment_id} well {well_id}"
         super().__init__(self.message)
+
+
+class OverFillException(Exception):
+    """Raised when a vessel if over filled"""
+
+    def __init__(self, name, volume, added_volume, capacity) -> None:
+        super().__init__(self)
+        self.name = name
+        self.volume = volume
+        self.added_volume = added_volume
+        self.capacity = capacity
+
+    def __str__(self) -> str:
+        return f"OverFillException: {self.name} has {self.volume} + {self.added_volume} > {self.capacity}"
+
+
+class OverDraftException(Exception):
+    """Raised when a vessel if over drawn"""
+
+    def __init__(self, name, volume, added_volume, capacity) -> None:
+        super().__init__(self)
+        self.name = name
+        self.volume = volume
+        self.added_volume = added_volume
+        self.capacity = capacity
+
+    def __str__(self) -> str:
+        return f"OverDraftException: {self.name} has {self.volume} + {self.added_volume} < 0"
