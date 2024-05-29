469f774a574c218d6f69cc80136f968f3fa6b279
Greg-Desktop <36201011+erasmus95@users.noreply.github.com>
2024-05-29 12:46:53 -0400

    class name change

--- epanda_lib/instrument_toolkit.py
+++ epanda_lib/instrument_toolkit.py
@@ -4,7 +4,7 @@ from logging import Logger
 from typing import Union
 
 from epanda_lib.mill_control import Mill, MockMill
-from epanda_lib.pump_control import MockPump, Pump
+from epanda_lib.pump_control import MockPump, SyringePump
 from epanda_lib.sartorius_local import Scale
 from epanda_lib.sartorius_local.mock import Scale as MockScale
 import epanda_lib.wellplate as wp
@@ -16,7 +16,7 @@ class Toolkit:
 
     mill: Union[Mill, MockMill]
     scale: Union[Scale, MockScale]
-    pump: Union[Pump, MockPump]
+    pump: Union[SyringePump, MockPump]
     wellplate: wp.Wellplate = None
     global_logger: Logger = None
     experiment_logger: Logger = None
