04ef80923d1e54de1ac946b8b48481de6be1881f
Greg-Laptop <36201011+erasmus95@users.noreply.github.com>
2024-06-25 10:38:21 -0400

    formatting
    get rid of old testing code

--- panda_lib/system state/json_exporter.py
+++ panda_lib/system state/json_exporter.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 # Load the JSON file into a pandas DataFrame
-df = pd.read_json('epanda_lib\system state\waste_status.json')
+df = pd.read_json('epanda_lib\\system state\\waste_status.json')
 
 # Save the DataFrame to a CSV file
-df.to_csv('epanda_lib\system state\waste_status.csv', index=False)
+df = pd.DataFrame
+df.to_csv('epanda_lib\\system state\\waste_status.csv', index=False)
