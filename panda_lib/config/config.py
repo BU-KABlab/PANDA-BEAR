b5e242e77cfff3209822e685e5700426dcf1da80
Greg-Laptop <36201011+erasmus95@users.noreply.github.com>
2024-07-01 18:37:22 -0400

    Refactor for config file validation

--- panda_lib/config/config.py
+++ panda_lib/config/config.py
@@ -1,10 +1,14 @@
 """The home of all project specific strings and values that are 
 better to be set in one place than to be scattered around the code."""
 
+from configparser import ConfigParser
 from pathlib import Path
-from os import environ
 import sqlite3
 
+
+configuration_path = Path(__file__).parent / "panda_sdl_config.ini"
+config = ConfigParser()
+config.read(configuration_path)
 
 def get_repo_path():
     """Returns the path of the repository."""
@@ -13,30 +17,6 @@ def get_repo_path():
     return repo_path
 
 
-def read_testing_config():
-    """Reads the testing configuration file."""
-    repo_path = get_repo_path()
-    config_path = repo_path / "config" / "testing.txt"
-    with open(config_path, "r", encoding="utf-8") as f:
-        return f.read().strip() == "True"
-
-
-def write_testing_config(value: bool):
-    """Writes the testing configuration file."""
-    repo_path = get_repo_path()
-    config_path = repo_path / "config" / "testing.txt"
-    with open(config_path, "w", encoding="utf-8") as f:
-        f.write(str(value))
-    environ["PANDA_SDL_TESTING"] = str(int(value))
-
-
-## Project default values
-AIR_GAP = float(40.0)  # ul
-DRIP_STOP = float(5.0)  # ul
-PURGE_VOLUME = float(20)  # ul
-RANDOM_FLAG = False  # True to randomize experiment order (grouped by priority)
-DEFAULT_PUMPING_RATE = float(0.3)  # ul/s
-
 ## Define local repository path - will also be used during testing
 LOCAL_REPO_PATH = Path(__file__).parents[2]
 
@@ -44,13 +24,10 @@ LOCAL_REPO_PATH = Path(__file__).parents[2]
 # testing.txt is the only place to change the mode besides from the main menue
 # The file is created if it does not exist
 
-if not (LOCAL_REPO_PATH / "panda_lib" / "config" / "testing.txt").exists():
-    write_testing_config(True)  # Default is testing mode
-
 ## Flag to use only local paths - can be changed while running the program
-TESTING_MODE_ACTIVE = read_testing_config()
+TESTING_MODE_ACTIVE = config.getboolean("OPTIONS", "testing")
 try:
-    TESTING_DIRECTORY = environ["PANDA_SDL_TESTING_PATH"]
+    TESTING_DIRECTORY = config.get("PATHS_TESTING","testing_dir")
 
     if TESTING_DIRECTORY in [None, "",'None']:
         raise KeyError
@@ -63,7 +40,7 @@ except KeyError:
 
 if not TESTING_MODE_ACTIVE:
     try:
-        PRODUCTION_DIRECTORY = environ["PANDA_SDL_EXTERNAL_PATH"]
+        PRODUCTION_DIRECTORY = config.get("PATHS_PRODUCTION","production_dir")
 
         if PRODUCTION_DIRECTORY in [None, "",'None']:
             raise KeyError
@@ -72,55 +49,31 @@ if not TESTING_MODE_ACTIVE:
     except KeyError:
         print("PANDA_SDL_EXTERNAL_PATH environment variable not set.")
         print("Switching to testing mode")
-        write_testing_config(True)
+        config.set("OPTIONS", "testing", "True")
+        config.write(open(configuration_path, "w",encoding="utf-8"))
         TESTING_MODE_ACTIVE = True
 
 
-## Project File Names
-__MILL_CONFIG_FILE_NAME = "mill_config.json"
-__WELLPLATE_CONFIG_FILE_NAME = "wellplate_location.json"
-
-## Project directory names
-__CODE = "panda_lib"
-__CONFIG = "config"
-__DATA = "data"
-__LOGS = "logs"
-__SYS_STATE = "system state"
-__IMAGES = "application_images"
-
 ## FLIR Camera related - must be a python 3.6 environment
 try:
-    PYTHON_360_PATH = environ["PANDA_SDL_PYTHON_360_PATH"]
+    PYTHON_360_PATH = config.get("PATHS_GENERAL", "python_360_path")
 
     if PYTHON_360_PATH in [None, "",'None']:
         raise KeyError
 
     PYTHON_360_PATH = Path(PYTHON_360_PATH)
-except KeyError:
-    raise ValueError("PANDA_SDL_PYTHON_360_PATH environment variable not set.")
+except KeyError as e:
+    raise ValueError("PANDA_SDL_PYTHON_360_PATH environment variable not set.") from e
 
-CAMERA_SCRIPT_PATH = Path(LOCAL_REPO_PATH / __CODE / "flir_camera" / "camera.py")
+CAMERA_SCRIPT_PATH = Path(LOCAL_REPO_PATH / "panda_lib" / "flir_camera" / "camera.py")
 
 ## Build complete paths for each project directory or file
 if TESTING_MODE_ACTIVE:
     # Directories
-    PATH_TO_CODE = LOCAL_REPO_PATH / __CODE
-    PATH_TO_CONFIG = PATH_TO_CODE / __CONFIG
-    PATH_TO_DATA = TESTING_DIRECTORY / __DATA
-    PATH_TO_LOGS = TESTING_DIRECTORY / __LOGS
-    PATH_TO_DATA = TESTING_DIRECTORY / __DATA
-    PATH_TO_LOGS = TESTING_DIRECTORY / __LOGS
-    PATH_TO_IMAGES = PATH_TO_CODE / __IMAGES
-
-    # Files
-    MILL_CONFIG = PATH_TO_CONFIG / __MILL_CONFIG_FILE_NAME
-    WELLPLATE_LOCATION = PATH_TO_CONFIG / __WELLPLATE_CONFIG_FILE_NAME
-    EPANDA_LOG = PATH_TO_LOGS / "ePANDA.log"
-    DATA_ZONE_LOGO = PATH_TO_IMAGES / "data_zone_logo.png"
 
     # DB
     try:
-        SQL_DB_PATH = environ["PANDA_SDL_TESTING_DB_PATH"]
+        SQL_DB_PATH = config.get("PATHS_TESTING","testing_db_address")
 
         if SQL_DB_PATH in [None, "",'None']:
             raise KeyError
@@ -135,49 +88,20 @@ if TESTING_MODE_ACTIVE:
     if not SQL_DB_PATH.exists():
         Path(SQL_DB_PATH).with_suffix(".db").touch()
         conn = sqlite3.connect(SQL_DB_PATH)
-        with open(LOCAL_REPO_PATH / "template_db.sql", "r") as f:
+        with open(LOCAL_REPO_PATH / "template_db.sql", "r",encoding="utf-8") as f:
             conn.executescript(f.read())
         conn.close()
 
-    ## Validate that all paths exist and create them if they don't
-    for path in [
-        PATH_TO_CODE,
-        PATH_TO_CONFIG,
-        PATH_TO_DATA,
-        PATH_TO_LOGS,
-        PATH_TO_DATA,
-        PATH_TO_LOGS,
-        PATH_TO_IMAGES,
-    ]:
-        path = Path(path)
-        if not path.exists():
-            path.mkdir()
-            print(f"Created {path}")
-
-else:  # Use external paths
-    # Directories
-    PATH_TO_CODE = LOCAL_REPO_PATH / __CODE
-    PATH_TO_CONFIG = PATH_TO_CODE / __CONFIG
-    PATH_TO_DATA = PRODUCTION_DIRECTORY / __DATA
-    PATH_TO_LOGS = PRODUCTION_DIRECTORY / __LOGS
-    PATH_TO_DATA = PRODUCTION_DIRECTORY / __DATA
-    PATH_TO_LOGS = PRODUCTION_DIRECTORY / __LOGS
-    PATH_TO_IMAGES = PATH_TO_CODE / __IMAGES
-
-    # Files
-    MILL_CONFIG = PATH_TO_CONFIG / __MILL_CONFIG_FILE_NAME
-    WELLPLATE_LOCATION = PATH_TO_CONFIG / __WELLPLATE_CONFIG_FILE_NAME
-    EPANDA_LOG = PATH_TO_LOGS / "ePANDA.log"
-    DATA_ZONE_LOGO = PATH_TO_IMAGES / "data_zone_logo.png"
-
+else:
     # DB
     try:
-        SQL_DB_PATH = environ["PANDA_SDL_PRODUCTION_DB_PATH"]
+        SQL_DB_PATH = config.get("PATHS_PRODUCTION","production_db_address")
 
         if SQL_DB_PATH in [None, "",'None']:
             raise KeyError
 
         SQL_DB_PATH = Path(SQL_DB_PATH)
+
     except KeyError:
         print("PANDA_SDL_PRODUCTION_DB_PATH environment variable not set in .env file.")
         print("Using a local db")
@@ -185,7 +109,7 @@ else:  # Use external paths
         if not Path(SQL_DB_PATH).with_suffix(".db").exists():
             Path(SQL_DB_PATH).with_suffix(".db").touch()
             conn = sqlite3.connect(SQL_DB_PATH)
-            with open(LOCAL_REPO_PATH / "template_db.sql", "r") as f:
+            with open(LOCAL_REPO_PATH / "template_db.sql", "r",encoding="utf-8") as f:
                 conn.executescript(f.read())
             conn.close()
 
@@ -195,18 +119,3 @@ else:  # Use external paths
         conn.close()
     except sqlite3.Error as e:
         raise f"Error connecting to database: {e}" from e
-
-    ## Validate that all paths exist and create them if they don't
-    for path in [
-        PATH_TO_CODE,
-        PATH_TO_CONFIG,
-        PATH_TO_DATA,
-        PATH_TO_LOGS,
-        PATH_TO_DATA,
-        PATH_TO_LOGS,
-        PATH_TO_IMAGES,
-    ]:
-        path = Path(path)
-        if not path.exists():
-            path.mkdir()
-            print(f"Created {path}")
