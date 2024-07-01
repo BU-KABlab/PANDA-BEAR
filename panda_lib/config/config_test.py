44fd295fc0714917e80597b134c5bc093aac225c
Greg-Laptop <36201011+erasmus95@users.noreply.github.com>
2024-07-01 18:54:48 -0400

    little script to print out the resolved paths

---
+++ panda_lib/config/config_test.py
@@ -0,0 +1,41 @@
+"""config test, read back the config file and interpret the paths"""
+
+from configparser import ConfigParser
+from pathlib import Path
+
+# Read the config file
+config_file = "./panda_lib/config/panda_sdl_config.ini"
+config = ConfigParser()
+config.read(config_file)
+
+
+local_dir = Path(__file__).parent.parent
+config.set("PATHS_GENERAL", "local_dir", str(local_dir))
+config.write(open(config_file, "w", encoding="utf-8"))
+config.read(config_file)
+
+# Print the config file values
+for section in config.sections():
+    print(f"[{section}]")
+    for key, value in config.items(section):
+
+        # Handle None or blank values
+        if value in [None, "", "None"]:
+            print(f"{key} = None")
+            continue
+
+        if "dir" in key or "path" in key:
+            try:
+                # Check if the path exists
+                assert Path(value).exists()
+            except AssertionError:
+                print(f"{key} = Path does not exist: {value}")
+                continue
+
+            # Generate complete path
+            complete_path = Path(value).resolve()
+            print(f"{key} = {complete_path}")
+
+        else:
+            print(f"{key} = {value}")
+    print()
