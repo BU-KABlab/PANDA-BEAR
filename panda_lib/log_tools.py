c5ed6e696e55dd2e762b24fd561a141fc9bec26a
Greg-Laptop <36201011+erasmus95@users.noreply.github.com>
2024-07-01 17:33:47 -0400

    Attempt to make central log formatter for the program that can still retain some customization per module

--- panda_lib/log_tools.py
+++ panda_lib/log_tools.py
@@ -1,19 +1,54 @@
 """
 Methods and classses related to logging for the ePANDA project
 """
+
 import logging
-from .config.config import EPANDA_LOG
-e_panda_logger = logging.getLogger("ePANDA")
-e_panda_logger.setLevel(logging.DEBUG)  # change to INFO to reduce verbosity
-formatter = logging.Formatter(
-    "%(asctime)s&%(name)s&%(levelname)s&%(module)s&%(funcName)s&%(lineno)d&&&&%(message)s&"
-)
-system_handler = logging.FileHandler(EPANDA_LOG)
-system_handler.setFormatter(formatter)
-e_panda_logger.addHandler(system_handler)
-console_logger = logging.StreamHandler()
-console_logger.setLevel(logging.INFO)
-e_panda_logger.addHandler(console_logger)
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config/panda_sdl_config.ini")
+
+if config.getboolean("OPTIONS", "testing"):
+    EPANDA_LOG = config.get("PATHS_TESTING", "logging_dir")
+else:
+    EPANDA_LOG = config.get("PATHS_PRODUCTION", "logging_dir")
+
+
+def setup_default_logger(
+    log_file=EPANDA_LOG,
+    log_name="panda_logger",
+    file_level=logging.DEBUG,
+    console_level=logging.INFO,
+):
+    """
+    Set up a logger with a file handler and a stream handler.
+
+    Args:
+        log_file (str): The path to the log file.
+        log_name (str): The name of the logger.
+
+    Returns:
+        logging.Logger: The logger.
+
+    """
+    logger = logging.getLogger(log_name)
+    logger.setLevel(file_level)  # change to INFO to reduce verbosity
+    formatter = logging.Formatter(
+        "%(asctime)s&%(name)s&%(levelname)s&%(module)s&%(funcName)s&%(lineno)d&&&&%(message)s&"
+    )
+    # The file handler will write to the log file
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # The stream handler will print to the console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(console_level)
+    logger.addHandler(console_handler)
+    return logger
+
+
+default_logger = setup_default_logger(EPANDA_LOG)
 
 
 class CustomLoggingFilter(logging.Filter):
@@ -22,7 +57,7 @@ class CustomLoggingFilter(logging.Filter):
     The values will be the experiment id and the well id
     """
 
-    def __init__(self, custom1, custom2, custom3,custom4):
+    def __init__(self, custom1, custom2, custom3, custom4):
         super().__init__()
         self.custom1 = custom1
         self.custom2 = custom2
