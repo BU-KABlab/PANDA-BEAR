84e08bae606c1558861b9da684062b10b75e2725
Panda-Station <36201011+erasmus95@users.noreply.github.com>
2024-09-25 15:36:56 -0400

    test running the slackbot on a seperate thread

--- slack_bot.py
+++ slack_bot.py
@@ -1,12 +1,21 @@
 """Script to starting and stopping the ePANDA Slack Bot"""
 
+import threading
 from panda_lib.slack_tools.SlackBot import SlackBot
 
-#pylint: disable=broad-exception-caught
-# choose_testing_mode = input("Testing? (y/n): ").strip().lower()
-choose_testing_mode = "y"
-TEST = True if choose_testing_mode[0] == "y" else False
+TEST = True
 
-bot = SlackBot(test=TEST)
-print("Starting Slack Bot")
-bot.run()
+def run_slack_bot():
+    """Run the SlackBot"""
+    bot = SlackBot(test=TEST)
+    print("Starting Slack Bot")
+    bot.run()
+
+# Create a thread to run the SlackBot
+slack_bot_thread = threading.Thread(target=run_slack_bot)
+
+# Start the thread
+slack_bot_thread.start()
+
+# Optionally, you can join the thread if you want the main program to wait for it to finish
+slack_bot_thread.join()
