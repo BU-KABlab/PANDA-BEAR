36b4485358831e49b65e253a2a3507dee53f9d82
erasmus95 <gregoryrobben@gmail.com>
2024-11-06 19:31:12 -0500

    add capping and decapping steps

--- testing_and_validation/pump_suction_test.py
+++ testing_and_validation/pump_suction_test.py
@@ -1,31 +1,36 @@
 """Test the syringe and pipette for retention and suction."""
+
 import time
+
 from panda_lib.actions import (
+    Instruments,
+    forward_pipette_v2,
     solution_selector,
     waste_selector,
-    forward_pipette_v2,
-    Instruments,
-)
-from panda_lib.controller import (
-    establish_system_state,
-    connect_to_instruments,
-    disconnect_from_instruments,
 )
 from panda_lib.correction_factors import correction_factor
+from panda_lib.experiment_loop import (
+    connect_to_instruments,
+    disconnect_from_instruments,
+    establish_system_state,
+)
 from panda_lib.utilities import input_validation
 
 purge = 40
 drip_stop = 5
 
+
 def main():
     """Test the syringe and pipette for retention and suction."""
     # Establish system state
-    stock, waste, _ = establish_system_state()
+    _, _, _ = establish_system_state()
 
     # Connect to instruments
     toolkit, _ = connect_to_instruments(False)
 
-    test_volume = input_validation("Enter the volume to test (uL): ", (float,int), 0, 1000)
+    test_volume = input_validation(
+        "Enter the volume to test (uL): ", (float, int), (0,200), 0, "Invalid volume."
+    )
 
     test_volume = correction_factor(test_volume)
 
@@ -37,6 +42,16 @@ def main():
 
         # Withdraw water, raise to z=0, and hold it for 60 seconds
         toolkit.pump.withdraw(purge)
+
+        toolkit.mill.safe_move(
+            test_solution.coordinates.x,
+            test_solution.coordinates.y,
+            test_solution.coordinates.z_top,
+            Instruments.DECAPPER,
+        )
+
+        toolkit.arduino.no_cap()
+
         toolkit.mill.safe_move(
             test_solution.coordinates.x,
             test_solution.coordinates.y,
@@ -46,6 +61,16 @@ def main():
         toolkit.pump.withdraw(test_volume, test_solution)
         toolkit.mill.move_to_safe_position()
         toolkit.pump.withdraw(drip_stop)
+
+        toolkit.mill.safe_move(
+            test_solution.coordinates.x,
+            test_solution.coordinates.y,
+            test_solution.coordinates.z_top,
+            Instruments.DECAPPER,
+        )
+
+        toolkit.arduino.ALL_CAP()
+        toolkit.mill.move_to_safe_position()
         time.sleep(15)
         input("Press enter to continue...")
         toolkit.mill.safe_move(
@@ -54,7 +79,9 @@ def main():
             test_waste.coordinates.z_top,
             Instruments.PIPETTE,
         )
-        toolkit.pump.infuse(test_volume,test_solution, test_waste, blowout_ul=drip_stop+purge)
+        toolkit.pump.infuse(
+            test_volume, test_solution, test_waste, blowout_ul=drip_stop + purge
+        )
         # Forward pipette
         # test_forward_pipette = input("Do a forward pipette test? (y/n): ")
         # if test_forward_pipette.lower() == "y":
