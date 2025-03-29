0bc60b8df2e0d4d4bc21605b8606c96cf117553d
gregory-robben <gregoryrobben@gmail.com>
2025-03-29 13:53:57 -0400

    add common serial port selection functions to shared utilities

---
+++ shared_utilities/__init__.py
@@ -0,0 +1,50 @@
+def get_ports():
+    """List all available ports"""
+    import os
+
+    import serial.tools.list_ports
+
+    if os.name == "posix":
+        ports = list(serial.tools.list_ports.grep("ttyUSB"))
+    elif os.name == "nt":
+        ports = list(serial.tools.list_ports.grep("COM"))
+    else:
+        raise OSError("Unsupported OS")
+    return [port.device for port in ports]
+
+
+def get_port_names():
+    """List all available port names"""
+    import os
+
+    import serial.tools.list_ports
+
+    if os.name == "posix":
+        ports = list(serial.tools.list_ports.grep("ttyUSB"))
+    elif os.name == "nt":
+        ports = list(serial.tools.list_ports.grep("COM"))
+    else:
+        raise OSError("Unsupported OS")
+    return [port.name for port in ports]
+
+
+def get_port_manufacturers():
+    """List all available port manufacturers"""
+    import os
+
+    import serial.tools.list_ports
+
+    if os.name == "posix":
+        ports = list(serial.tools.list_ports.grep("ttyUSB"))
+    elif os.name == "nt":
+        ports = list(serial.tools.list_ports.grep("COM"))
+    else:
+        raise OSError("Unsupported OS")
+    return [port.manufacturer for port in ports]
+
+
+__all__ = [
+    "get_ports",
+    "get_port_names",
+    "get_port_manufacturers",
+]
