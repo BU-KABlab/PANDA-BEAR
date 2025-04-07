838afbd93d8d1a00e777d8d041c43672dcc496f6
gregory_robben <gregoryrobben@gmail.com>
2025-04-07 00:18:53 -0400

    remove unused pin code

--- panda_lib/sql_tools/sql_system_state.py
+++ panda_lib/sql_tools/sql_system_state.py
@@ -6,7 +6,7 @@ import datetime
 
 from pytz import utc
 
-from panda_lib.sql_tools.panda_models import SystemStatus, SystemVersions
+from panda_lib.sql_tools.panda_models import SystemStatus
 from panda_lib.utilities import SystemState
 from shared_utilities.config.config_tools import read_config
 from shared_utilities.db_setup import SessionLocal
@@ -14,30 +14,6 @@ from shared_utilities.db_setup import SessionLocal
 config = read_config()
 
 TESTING = config.getboolean("OPTIONS", "testing")
-
-
-def get_current_pin() -> int:
-    """
-    Get the current pin from the system_status table.
-
-    Returns:
-        int: The current pin.
-    """
-    with SessionLocal() as session:
-        # result = session.execute(
-        #     "SELECT pin FROM system_status ORDER BY status_time DESC LIMIT 1"
-        # )
-        # for row in result:
-        #     return row[0]
-
-        result = (
-            session.query(SystemVersions.pin)
-            .order_by(SystemVersions.id.desc())
-            .limit(1)
-            .all()
-        )
-
-        return result[0][0] if result else None
 
 
 def select_system_status(look_back: int = 1) -> SystemState:
