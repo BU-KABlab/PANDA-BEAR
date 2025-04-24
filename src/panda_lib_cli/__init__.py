ef7302f0126ffd6edff4b420a8648b26ace979dc
Greg <gregoryrobben@gmail.com>
2025-04-24 14:21:00 -0400

    fix cli init file

--- src/panda_lib_cli/__init__.py
+++ src/panda_lib_cli/__init__.py
@@ -1,10 +1,13 @@
 
 from .menu import main_menu
+from .menu.main_menu import main_menu as MainMenu
 from .hardware_calibration import decapper_testing, line_break_validation, mill_calibration_and_positioning
 
-__all__ = ["
+__all__ = [
     "main_menu",
     "decapper_testing",
     "line_break_validation",
     "mill_calibration_and_positioning",
+    "MainMenu",
+    "main_menu",
 ]
