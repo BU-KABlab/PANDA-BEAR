21e5673a231f78a88baa25c496bd5ea7e627eb40
gregory-robben <gregoryrobben@gmail.com>
2025-04-28 22:33:14 -0400

    work on common pipette interface

--- src/panda_lib/hardware/panda_pipettes/__init__.py
+++ src/panda_lib/hardware/panda_pipettes/__init__.py
@@ -2,25 +2,28 @@
 
 from shared_utilities.config.config_tools import read_config_value
 
+from .sql_pipette import Pipette as PipetteModel
+from .sql_pipette import (
+    activate_pipette,
+    deincrement_use_count,
+    insert_new_pipette,
+    select_current_pipette_id,
+    select_current_pipette_uses,
+    select_pipette_status,
+    update_pipette_status,
+)
+from .state import PipetteState
+
 pipette_type = read_config_value("PIPETTE", "PIPETTE_TYPE")
 if pipette_type == "WPI":
     from .wpi_syringe.pipette import Pipette
-    from .wpi_syringe.sql_pipette import Pipette as PipetteModel
-    from .wpi_syringe.sql_pipette import (
-        activate_pipette,
-        deincrement_use_count,
-        insert_new_pipette,
-        select_current_pipette_id,
-        select_current_pipette_uses,
-        select_pipette_status,
-        update_pipette_status,
-    )
-    from .wpi_syringe.state import PipetteState
+
 elif pipette_type == "OT2":
-    pass
+    from .ot2_pipette.pipette import Pipette
 
 else:
     raise ValueError(f"Invalid pipette type: {pipette_type}")
+
 
 __all__ = [
     "Pipette",
