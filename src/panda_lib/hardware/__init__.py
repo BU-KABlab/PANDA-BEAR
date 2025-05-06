ebc8c9dd700e2df416e7e369979402b84260f2e4
Greg <gregoryrobben@gmail.com>
2025-05-06 10:27:36 -0400

    formatting

--- src/panda_lib/hardware/__init__.py
+++ src/panda_lib/hardware/__init__.py
@@ -1,6 +1,6 @@
 from .arduino_interface import ArduinoLink, MockArduinoLink
 from . import grbl_cnc_mill as mill
- 
+
 __all__ = [
     "ArduinoLink",
     "MockArduinoLink",
--- src/panda_lib/hardware/interfaces.py
+++ src/panda_lib/hardware/interfaces.py
@@ -1,5 +1,6 @@
 class CameraInterface:
     """Abstract interface for camera operations"""
+
     def capture_image(self, *args, **kwargs):
         raise NotImplementedError
 
