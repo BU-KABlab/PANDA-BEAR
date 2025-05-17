ca6654e0bd306975937a2e9715433f8bdc009b4d
gregory_robben <gregoryrobben@gmail.com>
2025-05-16 21:04:21 -0400

    add a synchronous wrapper for the sartorius scale

--- pipette_validation_v2.py
+++ pipette_validation_v2.py
@@ -1,33 +1,27 @@
 import panda_lib
-from panda_lib.hardware import ArduinoLink, PandaMill, Scale
+from panda_lib.hardware import ArduinoLink, PandaMill
+from panda_lib.hardware import Scale
 from panda_lib.hardware.panda_pipettes import Pipette, insert_new_pipette
 from panda_lib.toolkit import Toolkit
-import asyncio
 from panda_lib.types import VialKwargs
 from panda_lib.labware.vials import StockVial, WasteVial
 import logging
 import pandas as pd
 logger = logging.getLogger("panda")
-async def tare_scale():
-    await tools.scale.zero()
-    print("Scale zeroed")
 
-async def read_scale():
-    # await tools.scale.zero()
-    reading = await tools.scale.get()
-    print(reading)
 insert_new_pipette(capacity=300)
 tools = Toolkit(
     mill=PandaMill(), # NOTE: The mill needs to be connected and homed before use
-    arduino=ArduinoLink("/dev/ttyACM1"),
-    scale=Scale("/dev/ttyACM0"),
+    arduino=ArduinoLink("/dev/ttyACM0"),
+    scale=Scale("/dev/ttyACM2"),
     global_logger=logger,
-
 )
 tools.pipette = Pipette(arduino=tools.arduino)
-tools.mill.set_feed_rate(2000) # TODO set back to 5000 for real test
+
+# Number of iterations to run
+n = 5  # Set a default value, update as needed
+
 readings = pd.DataFrame(columns=["Timestamp","Reading"])
-n = 1 # TODO update with the number of itterations
 vkwargs_src = VialKwargs(
             category=0,
             name="H20",
@@ -79,15 +73,17 @@ vial_dest = WasteVial(
 )
 vial_dest.save()
 # tare the scale
-asyncio.run(tare_scale())
+tools.scale.tare()
 
-readings.loc[0] = [pd.Timestamp.now(), asyncio.run(read_scale())]
+# Get initial reading
+readings.loc[0] = [pd.Timestamp.now(), tools.scale.get()]
 
 
 try:
     # With all objects created lets connect and home the mill
     tools.mill.connect_to_mill()
     tools.mill.homing_sequence()
+    tools.mill.set_feed_rate(2000) # TODO set back to 5000 for real test
 
     # Now lets iterate over the number of times we want to transfer
     # the vial contents and read the scale
@@ -99,14 +95,13 @@ try:
             toolkit=tools,
         )
         # TODO Transfer the vial to the scale
-        readings.loc[i+1] = [pd.Timestamp.now(), asyncio.run(read_scale())]
+        readings.loc[i+1] = [pd.Timestamp.now(), tools.scale.get()]
         # TODO Transfer the vial back to the holder
 
 finally:
     tools.disconnect()
 
     # Save the readings to a CSV file
-    df = pd.DataFrame(readings, columns=["Reading"])
     timestamp = pd.Timestamp.now().strftime("%Y-%m-%d %H:%M:%S")
-    df.to_csv(f"{timestamp}_scale_readings.csv", index=False)
-    print("Scale readings saved to scale_readings.csv")
+    readings.to_csv(f"{timestamp}_scale_readings.csv", index=False)
+    print(f"Scale readings saved to {timestamp}_scale_readings.csv")
